dbe43da3e99960b259ec3a339ab4ece65d803e3f
Jordan Hagedorn <hagedornjordan@gmail.com>
2022-03-15 00:14:22 -0400

    add login link

--- client/client/components/navHeader.js
+++ client/client/components/navHeader.js
@@ -1,7 +1,7 @@
 import axiosInstance from "../helpers/axios";
 import logout from "../helpers/auth";
 import Link from "next/link";
-const NavHeader = (props) => {
+const NavHeader = props => {
   const username = props.user;
   return (
     <div className="flex justify-start min-w-full pl-4 pb-3 mb-5 bg-primary border-b-2">
@@ -9,6 +9,9 @@ const NavHeader = (props) => {
         Livestock Trader
       </h1>
       <Link href="/forSale"> Listings</Link>
+      <div className="ml-auto">
+        <Link href="/adminDashboard"> Login </Link>
+      </div>
     </div>
   );
 };
